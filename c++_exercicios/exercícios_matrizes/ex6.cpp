f05a26d6e4a271ad3c18357c72ab053a225be189
Cauã Domingos <102312253+cauadomingos72@users.noreply.github.com>
2022-05-25 10:48:26 -0300

    Create ex6.cpp

---
+++ c++_exercicios/exercícios_matrizes/ex6.cpp
@@ -0,0 +1,54 @@
+////////////////////////////////////////////////////////////////////////////////////
+// Faça um programa que solicita ao usuário 16 valores reais e armazene em uma matriz 4x4. O programa deve somar os elementos de cada uma das linhas armazenando o resultado da soma em um vetor. A seguir, deve multiplicar cada elemento da matriz pela soma da sua respectiva linha. Exiba na tela a matriz resultante.
+////////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <locale.h>
+#include <time.h>
+#define TAM 4
+
+using namespace std;
+
+int main() {
+  setlocale (LC_ALL, "Portuguese");
+
+  srand (time(NULL));
+  int mat[TAM][TAM], vet[TAM], lin, col, soma=0, resulX=1;;
+
+  cout << "Digite 16 valores:" << endl;
+  for (lin = 0; lin < TAM; lin++) {
+    for (col = 0; col < TAM; col++) {
+      mat[lin][col] = rand() % (TAM*TAM) +1;
+    }
+  }
+  cout << endl;
+
+  for (lin = 0; lin < TAM; lin++) {
+    for (col = 0; col < TAM; col++) {
+      cout << mat[lin][col] << "\t";
+    }
+    cout << endl;
+  }
+  
+  cout << endl;
+  
+  for (lin = 0; lin < TAM; lin++) {
+    for (col = 0; col < TAM; col++) {
+      soma = mat[lin][col] + soma;
+    }
+    vet[lin] = soma;
+    soma = 0;
+  }
+
+  for (lin = 0; lin < TAM; lin++) {
+    cout << vet[lin] << "\t\n";
+  }
+  
+  cout << endl;
+  
+  for (lin = 0; lin < TAM; lin++) {
+    resulX = vet[lin] * resulX;
+  }
+
+  cout << "A Multiplicação das Somas das linhas é: " << resulX << endl;
+}
