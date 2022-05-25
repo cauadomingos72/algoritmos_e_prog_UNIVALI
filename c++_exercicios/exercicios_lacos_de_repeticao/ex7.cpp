7e9e57c21db511a90db056ca68179c274afea27e
Cauã Domingos <102312253+cauadomingos72@users.noreply.github.com>
2022-05-25 07:35:57 -0300

    Create ex7.cpp

---
+++ c++_exercicios/exercicios_lacos_de_repeticao/ex7.cpp
@@ -0,0 +1,13 @@
+// Faça um programa que solicita ao usuário uma quantidade indeterminada de números inteiros. O programa deve calcular e escrever a média aritmética apenas dos números pares. A entrada de dados deve ser encerrada quando o número 0 (ZERO) for digitado
+
+#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int main() {
+  setlocale (LC_ALL, "Portuguese")
+
+  int med, qtd, num, 
+
+}
