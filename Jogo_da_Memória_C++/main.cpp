d5050b88fb4d745c43fb52f3fe9232bf5b47dd21
Cauã Domingos <102312253+cauadomingos72@users.noreply.github.com>
2022-06-08 10:27:33 -0300

    Update main.cpp

--- Jogo_da_Memória_C++/main.cpp
+++ Jogo_da_Memória_C++/main.cpp
@@ -19,11 +19,26 @@ using namespace std;
 
 int main() {
   srand(time(NULL));
-  int matP[TAM][TAM] {1,4,5,2,7,2,8,7,3,6,1,4,6,5,3,8} , matT[TAM][TAM], matIL[TAM][TAM], matIC[TAM][TAM], lin, col;
+  int matP[TAM][TAM] {1,4,5,2,7,2,8,7,3,6,1,4,6,5,3,8} , matT[TAM][TAM]{}, matIL[TAM][TAM] {}, matIC[TAM][TAM] {}, lin, col;
 
-  for (lin=0;lin<TAM;lin++){
+    for(lin=0;lin<TAM;lin++) {
+      for(col=0;col<TAM;col++) {
+        matT[lin][col] = matP[col][lin];
+      }
+    }
+    
+    for (lin=0;lin<TAM;lin++){      // Exibindo a matriz principal [remover no final]
         for (col=0;col<TAM;col++){
             cout << matP[lin][col] << "\t";
+        }
+        cout << endl;
+    }
+
+    cout<<endl;
+
+    for (lin=0;lin<TAM;lin++){      // Exibindo a matriz transposta [remover no final]
+        for (col=0;col<TAM;col++){
+            cout << matT[lin][col] << "\t";
         }
         cout << endl;
     }
