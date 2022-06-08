e322e79d3bb9cbefbbea69284209d5761c2452ef
Cauã Domingos <102312253+cauadomingos72@users.noreply.github.com>
2022-06-08 10:20:04 -0300

    Update main.cpp

--- Jogo_da_Memória_C++/main.cpp
+++ Jogo_da_Memória_C++/main.cpp
@@ -1,3 +1,15 @@
+/** 
+  Já implementado:
+
+  O que falta:
+    -> Criar a matriz transposta
+    -> Criar a matriz invertida por linha
+    -> Criar a matriz invertida por coluna
+    -> Criar um seletor da matriz gabarito
+    ->
+
+*/
+
 #include <iostream>
 #include <locale.h>
 #include <time.h>
@@ -7,24 +19,7 @@ using namespace std;
 
 int main() {
   srand(time(NULL));
-  int matP[TAM][TAM], matT[TAM][TAM], matIL[TAM][TAM], matIC[TAM][TAM], lin, col;
-
-  matP[0][0]=1;
-  matP[0][1]=4;
-  matP[0][2]=5;
-  matP[0][3]=2;
-  matP[1][0]=7;
-  matP[1][1]=2;
-  matP[1][2]=8;
-  matP[1][3]=7;
-  matP[2][0]=3;
-  matP[2][1]=6;
-  matP[2][2]=1;
-  matP[2][3]=4;
-  matP[3][0]=6;
-  matP[3][1]=5;
-  matP[3][2]=3;
-  matP[3][3]=8;
+  int matP[TAM][TAM] {1,4,5,2,7,2,8,7,3,6,1,4,6,5,3,8} , matT[TAM][TAM], matIL[TAM][TAM], matIC[TAM][TAM], lin, col;
 
   for (lin=0;lin<TAM;lin++){
         for (col=0;col<TAM;col++){
