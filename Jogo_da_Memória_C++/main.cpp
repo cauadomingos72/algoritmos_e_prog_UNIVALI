6489c6c1a34b5585e22a54583d75a2d4a5a78449
Cauã Domingos <102312253+cauadomingos72@users.noreply.github.com>
2022-06-08 10:13:36 -0300

    Update main.cpp

--- Jogo_da_Memória_C++/main.cpp
+++ Jogo_da_Memória_C++/main.cpp
@@ -7,5 +7,29 @@ using namespace std;
 
 int main() {
   srand(time(NULL));
-  int matP[TAM][TAM], lin, col;
+  int matP[TAM][TAM], matT[TAM][TAM], matIL[TAM][TAM], matIC[TAM][TAM], lin, col;
+
+  matP[0][0]=1;
+  matP[0][1]=4;
+  matP[0][2]=5;
+  matP[0][3]=2;
+  matP[1][0]=7;
+  matP[1][1]=2;
+  matP[1][2]=8;
+  matP[1][3]=7;
+  matP[2][0]=3;
+  matP[2][1]=6;
+  matP[2][2]=1;
+  matP[2][3]=4;
+  matP[3][0]=6;
+  matP[3][1]=5;
+  matP[3][2]=3;
+  matP[3][3]=8;
+
+  for (lin=0;lin<TAM;lin++){
+        for (col=0;col<TAM;col++){
+            cout << matP[lin][col] << "\t";
+        }
+        cout << endl;
+    }
 }
