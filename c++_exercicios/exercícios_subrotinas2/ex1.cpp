fdbe3a79d9e661aa4a9843985655bacf90e67b74
Cauã Domingos <102312253+cauadomingos72@users.noreply.github.com>
2022-08-09 09:12:39 -0300

    Update ex1.cpp

--- c++_exercicios/exercícios_subrotinas2/ex1.cpp
+++ c++_exercicios/exercícios_subrotinas2/ex1.cpp
@@ -1,3 +1,32 @@
 /**
 Faça uma função que recebe três números reais por parâmetro e retorne o menor deles. 
 */
+#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+float verf(float a){
+  float num, menor=0;
+  for(int i=0;i<3;i++){
+    cout<<"Digite um valor qualquer:"<<endl;
+    cin>>num;
+    if(menor==0){
+      menor=num;
+    }
+    if(menor>num){
+      menor=num;
+    }
+  }
+  return menor;
+}
+
+int main() {
+  setlocale (LC_ALL,"Portuguese");
+
+  float num,res;
+
+  res = verf(num);
+  
+  cout<<"O menor número digitado é: "<<res<<endl;
+}
