a6755e95450e050fdb082ca85626aa35346b55ff
Cauã Domingos <102312253+cauadomingos72@users.noreply.github.com>
2022-08-16 10:12:29 -0300

    Create ex3.cpp

---
+++ c++_exercicios/exercícios_subrotinas3/ex3.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <locale.h>
+#define TAM 5
+
+using namespace std;
+
+int maior(int vet[TAM]){
+  int cont=0;
+  for(int i=0;i<TAM;i++){
+    if(vet[0]<vet[i]){
+      cont++;
+    }
+  }
+  return cont;
+}
+
+int menor(int vet[TAM]){
+  int cont=0;
+  for(int i=0;i<TAM;i++){
+    if(vet[0]>vet[i]){
+      cont++;
+    }
+  }
+  return cont;
+}
+
+int igual(int vet[TAM]){
+  int cont=0;
+  for(int i=0;i<TAM;i++){
+    if(vet[0]==vet[i]){
+      cont++;
+    }
+  }
+  return cont;
+}
+
+int main() {
+  setlocale (LC_ALL, "Portuguese");
+  srand(time(NULL));
+  int vet[TAM];
+
+  for(int i=0;i<TAM;i++){
+    vet[i]=rand()%6+1;
+    cout<<vet[i]<<"\t";
+  }
+
+  cout<<endl;
+  cout<<"Quantidade de números maiores que "<<vet[0]<<" é de: "<<maior(vet)<<endl;
+  cout<<"Quantidade de números menores que "<<vet[0]<<" é de: "<<menor(vet)<<endl;
+  cout<<"Quantidade de números iguais a "<<vet[0]<<" é de: "<<igual(vet)<<endl;
+  
+}
