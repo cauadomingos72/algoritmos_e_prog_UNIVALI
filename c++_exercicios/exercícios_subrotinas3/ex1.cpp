d1d9d71c6d4287141995ad431110bc3159ca6e87
Cauã Domingos <102312253+cauadomingos72@users.noreply.github.com>
2022-08-16 09:10:36 -0300

    Create ex1.cpp

---
+++ c++_exercicios/exercícios_subrotinas3/ex1.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <locale.h>
+#define TAM 10
+
+using namespace std;
+
+void leitura(int vet[TAM]){
+  for (int i=0;i<TAM;i++){
+    cout<<vet[i]<<"\t";
+  }
+}
+
+float media(int vet[TAM]){
+  int soma=0;
+  for(int i=0;i<TAM;i++){
+    soma=soma+vet[i];
+  }
+  return soma/TAM;
+}
+
+int maior(int vet[TAM]){
+  int maior;
+  for(int i=0;i<TAM;i++){
+    if(i==0){
+      maior=vet[i];
+    }
+    if(vet[i]>maior){
+      maior=vet[i];
+    }
+  }
+  return maior;
+}
+
+int menor(int vet[TAM]){
+  int menor;
+  for(int i=0;i<TAM;i++){
+    if(i==0){
+      menor=vet[i];
+    }
+    if(vet[i]<menor){
+      menor=vet[i];
+    }
+  }
+  return menor;
+}
+
+int main() {
+  setlocale (LC_ALL, "Portuguese");
+  srand(time(NULL));
+  int vet[TAM];
+
+  for (int i=0;i<TAM;i++){
+    vet[i]=rand()%10+1;
+  }
+
+  leitura(vet);
+
+  cout<<endl;
+  cout<<"Maior Valor: "<<maior(vet)<<endl;
+  cout<<"Menor Valor: "<<menor(vet)<<endl;
+  cout<<"Média: "<<media(vet)<<endl;
+}
