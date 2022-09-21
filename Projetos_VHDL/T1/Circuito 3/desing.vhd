48f4f91862d740e36cd08818eec8127945531fd8
Cauã Domingos <102312253+cauadomingos72@users.noreply.github.com>
2022-09-21 08:02:45 -0300

    Update desing.vhd

--- Projetos_VHDL/T1/Circuito 3/desing.vhd
+++ Projetos_VHDL/T1/Circuito 3/desing.vhd
@@ -1,16 +1,16 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity codi2_hot is
+entity cod2one_4bit is
 port(  i_A : in std_logic;
        i_B : in std_logic;
-       o_S : out bit_vector(3 downto 0));
-end codi2_hot;
+       o_S : out std_logic_vector(3 downto 0));
+end cod2one_4bit;
 
-architecture arch_1 of codi2_hot is
+architecture arch_1 of cod2one_4bit is
 begin
-  o_S(0) <= (not i_A and not i_B);
-  o_S(1) <= (not i_A and i_B);
-  o_S(2) <= (i_A and not i_B);
-  o_S(3) <= (i_A and i_B);
+    	o_S(0) <= (not i_A and not i_B);
+    	o_S(1) <= (not i_A and i_B);
+    	o_S(2) <= (i_A and not i_B);
+    	o_S(3) <= (i_A and i_B);
 end arch_1;
