a6343999b68a2f099c3ae5b0966bf09a3cde5560
Cauã Domingos <102312253+cauadomingos72@users.noreply.github.com>
2022-09-21 08:02:22 -0300

    Update testbench.vhd

--- Projetos_VHDL/T1/Circuito 2/testbench.vhd
+++ Projetos_VHDL/T1/Circuito 2/testbench.vhd
@@ -1,56 +1,54 @@
-library IEEE;
-use IEEE.std_logic_1164.all;
+library ieee;
+use ieee.std_logic_1164.all;
 
-entity tb_demux2_1bit is
---empty
-end tb_demux2_1bit;
+entity tb_demux1_2bit is
+end tb_demux1_2bit;
 
-architecture arch_1 of tb_demux2_1bit is
-
---DUT component
-component demux2_1bit is
-port( i_SEL : in std_logic;
-      i_A : in std_logic;
-      o_R : out std_logic;
-      o_S : out std_logic);
+architecture arch_1 of tb_demux1_2bit is
+component demux1_2bit is
+port ( i_SEL : in std_logic; -- Seletor de entradas a ser usada
+		i_S : in std_logic; -- Entrada
+		o_A : out std_logic; -- Saída A
+		o_B : out std_logic); -- Saída B
 end component;
 
-signal w_SEL, w_A, w_R, w_S : std_logic;
+signal w_SEL, w_S, w_A, w_B : std_logic;
 
 begin
-  --Connect DUT
-  u_DUT: demux2_1bit port map(i_SEL => w_SEL,
-                              i_A => w_A,
-                              o_R => w_R,
-                              o_S => w_S);
-  process
-  begin
-    --Select output channel R
-    w_SEL  <='0';
-    w_A    <='0';
-    wait for 1 ns;
-    assert(w_R='0' and w_S='0') report "Fail @ 00" severity error;
+      	-- Connect DUT
+      	u_DUT: demux1_2bit port map(i_SEL => w_SEL,
+			                        i_S => w_S,
+			                        o_A => w_A,
+			                        o_B => w_B);
+process
+begin
+		-- Casos para que a chave seletora esteja em 0
+		w_SEL <= '0';
+        w_S <='0';
+        wait for 1 ns;
+        assert(w_A = '0' and w_B = '0') report "Fail @ 00" severity error;
     
-    w_SEL  <='0';
-    w_A    <='1';
-    wait for 1 ns;
-    assert(w_R='1' and w_S='0') report "Fail @ 01" severity error;
+        w_SEL <= '0';
+        w_S <='1';
+        wait for 1 ns;
+        assert(w_A = '1' and w_B = '0') report "Fail @ 10" severity error;
+
+        -- Casos para que a chave seletora esteja em 1
+        w_SEL <= '1';
+        w_S <='0';
+        wait for 1 ns;
+        assert(w_A = '0' and w_B = '0') report "Fail @ 00" severity error;
     
-    -- Select output channel S
-    w_SEL  <='1';
-    w_A    <='0';
-    wait for 1 ns;
-    assert(w_R='0' and w_S='0') report "Fail @ 10" severity error;
-    
-    w_SEL  <='1';
-    w_A    <='1';
-    wait for 1 ns;
-    assert(w_R='0' and w_S='1') report "Fail @ 11" severity error;
-    
-    -- Clear inputs
-    w_SEL  <='0';
-    w_A    <='0';
-    assert false report "Test Done!" severity error;
-    wait;
-  end process;
+
+        w_SEL <= '1';
+        w_S <='1';
+        wait for 1 ns;
+        assert(w_A = '0' and w_B = '1') report "Fail @ 01" severity error;
+
+        -- Para limpar as entradas
+        w_SEL <= '0';
+        w_S <= '0';
+        assert false report "Test done." severity note;
+        wait;
+	end process;
 end arch_1;
