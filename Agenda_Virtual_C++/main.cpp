d24648254194c3039406e043a7e8fb33acb6c7e7
Vinícius Henrique Ribeiro <102866417+vini-ribeiro@users.noreply.github.com>
2022-10-13 09:45:49 -0300

    Update main.cpp

--- Agenda_Virtual_C++/main.cpp
+++ Agenda_Virtual_C++/main.cpp
@@ -1,30 +1,27 @@
 #include <iostream>
 #include <string>
 #include <cstdio>
-#include <locale.h>
 #include "windows.h"
-#include <cmath>
 #include "Agenda.hpp"
-
 
 using namespace std;
 
 ostream& operator<<(ostream &out, const Pessoal &p)
 {
-    out<<p.ID<<endl;
-    out<<p.nome<<endl;
-    out<<p.CPF<<endl;
-    out<<p.celular<<endl;
+    out<<"ID: "<<p.ID<<endl;
+    out<<"Nome: "<<p.nome<<endl;
+    out<<"CPF: "<<p.CPF<<endl;
+    out<<"Celular: "<<p.celular<<endl;
 
     return out;
 }
 
 ostream& operator<<(ostream &out, const Comercial &p)
 {
-    out<<p.ID<<endl;
-    out<<p.nome<<endl;
-    out<<p.CNPJ<<endl;
-    out<<p.telefone<<endl;
+    out<<"ID: "<<p.ID<<endl;
+    out<<"Nome: "<<p.nome<<endl;
+    out<<"CNPJ: "<<p.CNPJ<<endl;
+    out<<"Telefone: "<<p.telefone<<endl;
 
     return out;
 }
@@ -33,9 +30,9 @@ int menu()
 {
     int op;
     do {
-        cout<<"1. inserir um novo item na agenda"<<endl;
-        cout<<"2. remover um item da agenda"<<endl;
-        cout<<"3. pesquisar um item na agenda"<<endl;
+        cout<<"1. inserir um novo item na agenda (sempre no final)"<<endl;
+        cout<<"2. remover um item da agenda (pelo ID ou nome)"<<endl;
+        cout<<"3. pesquisar um item na agenda (por ID ou nome)"<<endl;
         cout<<"4. ordenar a agenda por nome"<<endl;
         cout<<"5. mostrar todos os itens "<<endl;
         cout<<"6. encerrar o programa"<<endl;
@@ -61,40 +58,36 @@ void registra_dados_comerciais(const char *frase, Comercial &dados)
 }
 
 template <typename T, int MAX>
-void procura_dados_pessoal(const char *frase, const Agenda<T,MAX> &ag)
+int procura_dados(const char *frase, const Agenda<T,MAX> &ag)
 {
-    string p;
-
+    int ID_produrado;
     cout<<frase<<endl;
-    cin>>p;
-    fflush(stdin);
-    for (int i=0; i<MAX; i++)
-    {
-        if(ag.itens[i].ID == p || ag.itens[i].nome == p)
-            cout<<ag.itens[i].ID<<" - "<<ag.itens[i].nome<<" - "<<ag.itens[i].CPF<<" - "<<ag.itens[i].celular;
+    cin>>ID_produrado;
+
+    for (int i=0; i<MAX; i++){
+        if (ag.itens[i].ID == ID_produrado)
+            return i;
     }
-
-
+    
+    return -1;
 }
 
-template <typename T, int MAX>
-void procura_dados_comercial(const char *frase, const Agenda<T,MAX> &ag)
-{
-    string p;
-    cout<<frase<<endl;
-    cin>>p;
-    for (int i=0; i<MAX; i++)
-    {
-        if(ag.itens[i].ID == p || ag.itens[i].nome == p || ag.itens[i].CNPJ == p || ag.itens[i].telefone == p)
-            cout<<ag.itens[i].ID<<" - "<<ag.itens[i].nome<<" - "<<ag.itens[i].CNPJ<<" - "<<ag.itens[i].telefone;
-    }
+void selecao(int &opcao) {
+    cout<<"1 - Pessoal"<<endl;
+    cout<<"2 - Comercial"<<endl;
+    cin>>opcao;
 }
 
-
+void selecao2(int &opcao, int posicao) {
+    if (posicao >= 0){
+        cout<<"Dado nao encontrado"<<endl;
+        cout<<"Tentar novamente? (1 = sim ou qualquer numero = nao)"<<endl;
+        cin>>opcao;
+    }
+}
 
 int main()
-{
-    setlocale(LC_ALL,"Portuguese");
+{   
     Agenda<Pessoal,100> agp;
     Agenda<Comercial,100> agc;
     Pessoal pessoa;
@@ -107,10 +100,7 @@ int main()
         switch (op){
             case 1:
                 int opcao;
-                cout<<"1 - Pessoal"<<endl;
-                cout<<"2 - Comercial"<<endl;
-                cin>>opcao;
-
+                selecao(opcao);
                 if (opcao == 1){
                     registra_dados_pessoais("Insira seu ID, nome, CPF e numero de celular respectivamente.", pessoa);
                     insere(agp, pessoa);
@@ -122,24 +112,41 @@ int main()
             break;
 
             case 2:
+                int opcao, posicao;
+                do {
+                    selecao(opcao);
+                    
+                    if (opcao == 1){
+                        posicao = procura_dados("Insira o dado que deseja remover da agenda pessoal (ID):", agp);
+                        if (posicao >= 0)
+                            remova(agp, agp.itens[posicao])
+                    }
+                    if (opcao == 2){
+                        posicao = procura_dados("Insira o dado que deseja remover da agenda comercial (ID):", agc);
+                        if (posicao >= 0)
+                            remova(agc, agc.itens[posicao])
+                    }
+                    selecao2(opcao, posicao);
+                } while (opcao == 1);
             break;
 
             case 3:
-                int op;
-                do
-                {
-                    cout<<"Escolha uma opcao de identificacao:"<<endl;
-                    cout<<"1 - Pessoal"<<endl;
-                    cout<<"2 - Comercial"<<endl;
-                    cin>>op;
-                    fflush(stdin);
-
-                    if(op==1)
-                        procura_dados_pessoal("Escreva o que você deseja buscar.", agp);
-                    if(op==2)
-                        procura_dados_comercial("Escreva o que você deseja buscar.", agc);
-                } while (op < 1 || op > 2);
-
+                int opcao, posicao;
+                do {
+                    selecao(opcao);
+                    
+                    if (opcao == 1){
+                        posicao = procura_dados("Insira o dado que deseja procurar na agenda pessoal:", agp);
+                        if (posicao >= 0)
+                            cout<<agp.itens[posicao]<<endl;
+                    }
+                    if (opcao == 2){
+                        posicao = procura_dados("Insira o dado que deseja procurar na agenda comercial:", agc);
+                        if (posicao >= 0)
+                            cout<<agc.itens[posicao]<<endl;
+                    }
+                    selecao2(opcao, posicao);
+                } while (opcao == 1);
             break;
 
             case 4:
