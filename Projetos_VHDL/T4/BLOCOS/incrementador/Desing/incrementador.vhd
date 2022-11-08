f36a8ba623e22d07090fcc69b5b77d06a915d43c
Cauã Domingos <102312253+cauadomingos72@users.noreply.github.com>
2022-11-08 19:37:12 -0300

    Update incrementador.vhd

--- Projetos_VHDL/T4/BLOCOS/incrementador/Desing/incrementador.vhd
+++ Projetos_VHDL/T4/BLOCOS/incrementador/Desing/incrementador.vhd
@@ -2,17 +2,17 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity incrementador is
-port ( i_A : in std_logic;
-	   i_B : in std_logic;
-       o_COUT : out std_logic;
-       o_S : out std_logic);
+port ( i_A : in std_logic_vector(3 downto 0);
+       o_S : out std_logic_vector(3 downto 0));
 end incrementador;
 
 architecture comp of incrementador is
 begin
-	process(i_A, i_B)
-    begin
-    o_S <= (i_A and i_B) or (not i_A and not i_B) or (not i_A and i_B) or (i_A and not i_B);
-    o_COUT <= (i_A and i_B);
-    end process;
+	process(i_A)
+	begin
+		o_S(3) <= ((not i_a(3) and i_a(2) and i_a(1) and i_a(0)) or (i_a(3) and not i_a(2)) or (i_a(3) and not i_a(1)) or (i_a(3) and not i_a(0)));
+		o_S(2) <= ((not i_a(2) and i_a(1) and i_a(0)) or (i_a(2) and not i_a(1)) or (i_a(2) and not i_a(0)));
+		o_S(1) <= ((not i_a(1) and i_a(0)) or (i_a(1) and not i_a(0)));
+		o_S(0) <= (not i_a(0));
+	end process;
 end comp;
