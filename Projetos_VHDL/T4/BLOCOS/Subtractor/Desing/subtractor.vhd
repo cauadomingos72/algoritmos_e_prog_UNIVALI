e4d632a15df7d4dc0e400e4b52b6aa7e20918feb
Cauã Domingos <102312253+cauadomingos72@users.noreply.github.com>
2022-11-08 09:26:31 -0300

    Update subtractor.vhd

--- Projetos_VHDL/T4/BLOCOS/Subtractor/Desing/subtractor.vhd
+++ Projetos_VHDL/T4/BLOCOS/Subtractor/Desing/subtractor.vhd
@@ -4,16 +4,16 @@ use IEEE.std_logic_1164.all;
 entity subtrator is
 port ( i_A : in std_logic;
 	   i_B : in std_logic;
-       i_WIN : in std_logic;
+       i_CIN : in std_logic;
        o_WOUT : out std_logic;
        o_S : out std_logic);
 end subtrator;
 
 architecture comp of subtrator is
 begin
-	process(i_A, i_B, i_WIN)
+	process(i_A, i_B, i_CIN)
     begin
-    o_S <= (i_A and i_B and i_WIN) or (not i_A and not i_B and i_WIN) or (not i_A and i_B and  not i_WIN) or (i_A and not i_B and not i_WIN);
-    o_WOUT <= ((i_A and i_B) or (i_A and i_WIN) or (i_B and i_WIN));
+    o_S <= (i_A and i_B and i_CIN) or (not i_A and not i_B and i_CIN) or (not i_A and i_B and  not i_CIN) or (i_A and not i_B and not i_CIN);
+    o_WOUT <= ((i_A and i_B) or (i_A and i_CIN) or (i_B and i_CIN));
     end process;
 end comp;
