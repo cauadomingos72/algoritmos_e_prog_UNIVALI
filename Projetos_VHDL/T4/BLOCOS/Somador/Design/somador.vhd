208aa7cb6a622d9c9eb0d714db058776737e5c82
Pedro H K Goncalves <108807271+PedroKons@users.noreply.github.com>
2022-11-08 09:16:36 -0300

    Update somador.vhd

--- Projetos_VHDL/T4/BLOCOS/Somador/Design/somador.vhd
+++ Projetos_VHDL/T4/BLOCOS/Somador/Design/somador.vhd
@@ -13,7 +13,7 @@ architecture comp of somador is
 begin
 	process(i_A, i_B, i_CIN)
     begin
-    o_S <= (i_A xor i_B xor i_CIN);
+    o_S <= (i_A and i_B and i_WIN) or (not i_A and not i_B and i_WIN) or (not i_A and i_B and  not i_WIN) or (i_A and not i_B and not i_WIN);
     o_COUT <= ((i_A and i_B) or (i_A and i_CIN) or (i_B and i_CIN));
     end process;
 end comp;
