0c6acda2bfc4acc7719dc33ffe9b06994aa7af9c
Cauã Domingos <102312253+cauadomingos72@users.noreply.github.com>
2022-11-16 11:36:11 -0300

    Update anot.vhd

--- Projetos_VHDL/T4/BLOCOS/Not/Desing/anot.vhd
+++ Projetos_VHDL/T4/BLOCOS/Not/Desing/anot.vhd
@@ -1,22 +1,13 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
-
 entity anot is
 port (i_A : in std_logic_vector(3 downto 0);
-		o_S : out std_logic_vector(3 downto 0));
+o_S : out std_logic_vector(3 downto 0));
 end anot;
-
 architecture comp of anot is
 begin
-
-
-   process(i_A)
-	begin
-		
-		o_S(0)<= not i_A(0);
-		o_S(1)<= not i_A(1);
-		o_S(2)<= not i_A(2);
-		o_S(3)<= not i_A(3);
-		
-	end process;
+o_S(0)<= not i_A(0);
+o_S(1)<= not i_A(1);
+o_S(2)<= not i_A(2);
+o_S(3)<= not i_A(3);
 end comp;
