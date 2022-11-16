035df757d31b2dd5a64f60f5b1ae84d16ac17912
Cauã Domingos <102312253+cauadomingos72@users.noreply.github.com>
2022-11-16 11:31:06 -0300

    Update fullincrementador.vhd

--- Projetos_VHDL/T4/BLOCOS/incrementador/Desing/fullincrementador.vhd
+++ Projetos_VHDL/T4/BLOCOS/incrementador/Desing/fullincrementador.vhd
@@ -1,27 +1,19 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
-use work.all;
 
-entity fullincrementador is
-port (i_A : in std_logic_vector (3 downto 0);
-		i_B : in std_logic;
-      o_COUT : out std_logic;
-      o_S : out std_logic_vector (3 downto 0));
-end fullincrementador;
-
-architecture design of fullincrementador is
-component incrementador
-port ( i_A : in std_logic;
-	    i_B : in std_logic;
-       o_COUT : out std_logic;
-       o_S : out std_logic);
-end component;
-
-signal w_B0, w_B1, w_B2 : std_logic;
-
+entity incrementador is
+port (  i_A : in std_logic_vector(3 downto 0);
+	o_S : out std_logic_vector(3 downto 0));
+end incrementador;
+	
+architecture comp of incrementador is
 begin
-FS0 : incrementador port map(i_A (0), i_B, w_B0, o_S (0));
-FS1 : incrementador port map(i_A (1), w_B0, w_B1, o_S (1));
-FS2 : incrementador port map(i_A (2), w_B1, w_B2, o_S (2));
-FS3 : incrementador port map(i_A (3), w_B2, o_COUT, o_S (3));
-end design;
+o_S(3) <= ((not i_a(3) and i_a(2) and i_a(1) and i_a(0)) or (i_a(3) and not i_a(2)) or (i_a(3) and not i_a(1)) or (i_a(3) and not i_a(0)));
+	
+o_S(2) <= ((not i_a(2) and i_a(1) and i_a(0)) or (i_a(2) and not i_a(1)) or (i_a(2) and not i_a(0)));
+	
+o_S(1) <= ((not i_a(1) and i_a(0)) or (i_a(1) and not i_a(0)));
+
+o_S(0) <= (not i_a(0));
+
+end comp;
