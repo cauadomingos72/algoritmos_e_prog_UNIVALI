0a2734f65fad3f004f87824387a91a5dae32f456
Cauã Domingos <102312253+cauadomingos72@users.noreply.github.com>
2022-11-16 11:32:13 -0300

    Update band.vhd

--- Projetos_VHDL/T4/BLOCOS/And/Desing/band.vhd
+++ Projetos_VHDL/T4/BLOCOS/And/Desing/band.vhd
@@ -2,23 +2,15 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity band is
-port (i_A : in std_logic_vector(3 downto 0);
-		i_B : in std_logic_vector(3 downto 0);
-		o_S : out std_logic_vector(3 downto 0));
+port (  i_A : in std_logic_vector(3 downto 0);
+	i_B : in std_logic_vector(3 downto 0);
+	o_S : out std_logic_vector(3 downto 0));
 end band;
-
+	
 architecture comp of band is
 begin
-
-
-   process(i_A,i_B)
-	begin
-		
-		o_S(0)<=(i_A(0) and i_B(0));
-		o_S(1)<=(i_A(1) and i_B(1));
-		o_S(2)<=(i_A(2) and i_B(2));
-		o_S(3)<=(i_A(3) and i_B(3));
-	end process;
+o_S(0)<=(i_A(0) and i_B(0));
+o_S(1)<=(i_A(1) and i_B(1));
+o_S(2)<=(i_A(2) and i_B(2));
+o_S(3)<=(i_A(3) and i_B(3));
 end comp;
-		
-		
