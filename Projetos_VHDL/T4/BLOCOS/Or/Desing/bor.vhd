b5830096a3fa5643d67e4631619ce7aa1296ad19
Cauã Domingos <102312253+cauadomingos72@users.noreply.github.com>
2022-11-16 11:32:52 -0300

    Update bor.vhd

--- Projetos_VHDL/T4/BLOCOS/Or/Desing/bor.vhd
+++ Projetos_VHDL/T4/BLOCOS/Or/Desing/bor.vhd
@@ -2,21 +2,15 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity bor is
-port (i_A : in std_logic_vector(3 downto 0);
-		i_B : in std_logic_vector(3 downto 0);
-		o_S : out std_logic_vector(3 downto 0));
+port (  i_A : in std_logic_vector(3 downto 0);
+	i_B : in std_logic_vector(3 downto 0);
+	o_S : out std_logic_vector(3 downto 0));
 end bor;
-
+	
 architecture comp of bor is
 begin
-
-
-   process(i_A,i_B)
-	begin
-		
-		o_S(0)<=(i_A(0) or i_B(0));
-		o_S(1)<=(i_A(1) or i_B(1));
-		o_S(2)<=(i_A(2) or i_B(2));
-		o_S(3)<=(i_A(3) or i_B(3));
-	end process;
+o_S(0)<=(i_A(0) or i_B(0));
+o_S(1)<=(i_A(1) or i_B(1));
+o_S(2)<=(i_A(2) or i_B(2));
+o_S(3)<=(i_A(3) or i_B(3));
 end comp;
