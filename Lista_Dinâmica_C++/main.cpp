84db2448ff6c536b3a4708a1c8728e5771e1c88c
Cauã Domingos <102312253+cauadomingos72@users.noreply.github.com>
2022-11-17 11:20:18 -0300

    Update main.cpp

--- Lista_Dinâmica_C++/main.cpp
+++ Lista_Dinâmica_C++/main.cpp
@@ -1,12 +1,69 @@
 #include <iostream>
 #include <fstream>
 #include <string.h>
-#include <windows.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
 #include "lista.h"
 
 using namespace std;
 
-int main()
+void menu (int &op)
 {
-    return 0;
+  cout<<"Menu:"<<endl;
+  cout<<"Adicionar (1)"<<endl;
+  cout<<"Remover - Código (2)"<<endl;
+  cout<<"Consulta - Código (3)"<<endl;
+  cout<<"Exibir (4)"<<endl;
+  cout<<"Encerrar (5)"<<endl;
+  cin>>op;
+}
+
+void extrai_dados(Produto &p)
+{
+  cout<<"Inira os dados do produto"<<endl;
+  cout<<"Nome: "<<endl;
+  fflush(stdin);
+  cin.ignore();
+  getline(cin, p.nome);
+  cout<<endl;
+  do {
+    cout<<"Código:"<<endl;
+    cin>>p.codigo;
+  } while (false);
+  cout<<endl;
+  cout<<"Preço:"<<endl;
+  cin>>p.preco;
+  /*system("clear");*/
+}
+
+int main() {
+  setlocale(LC_ALL, "Portuguese");
+  ListaE lista;
+  inicializa_lista(lista);
+  Produto item;
+  int op;
+
+  menu(op);
+
+  switch (op)
+  {
+    case 1:
+      extrai_dados(item);
+      insere_elemento_lista(lista, item);      
+    break;
+    
+    case 2:
+    break;
+  
+    case 3:
+    break;
+
+    case 4:
+    break;
+
+    default:
+    break;
+  }
+  return 0;
 }
