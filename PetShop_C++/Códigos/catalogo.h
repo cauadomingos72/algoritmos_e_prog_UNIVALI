244b5c27e9550367b21de364fd231b8b49eb1da2
Cauã Domingos <102312253+cauadomingos72@users.noreply.github.com>
2023-03-24 12:16:39 -0300

    Create catalogo.h

---
+++ PetShop_C++/Códigos/catalogo.h
@@ -0,0 +1,38 @@
+#ifdef CATALOGO_H_INCLUDED
+#define CATALOGO_H_INCLUDED
+
+
+class CatalogoDeProdtos
+{
+
+}
+class Produto
+{
+  string nome;
+  int cod;
+  float preco;
+public:
+  void getNome()
+  {
+    
+  }
+  void getPreco()
+  {
+    
+  }
+  void getCodigo()
+  {
+    
+  }
+  void getTipo()
+  {
+    
+  }
+}
+
+class ProdutoDesconto: public Produto
+{
+  int desconto
+}
+
+#endif 
