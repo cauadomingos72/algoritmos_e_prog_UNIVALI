f74936effde569171b1b12fe4fb0740eb18b5d5d
Cauã Domingos <102312253+cauadomingos72@users.noreply.github.com>
2023-03-24 12:15:37 -0300

    Create main.cpp

---
+++ PetShop_C++/Códigos/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <locale.h>
+#include <stdio.h>
+#include <string>
+#include "catalogo.h"
+#define MAX 10
+
+using namespace std;
+
+class PetShop
+{
+  public:
+  
+};
+
+
+int main() {
+  setlocale(LC_ALL, "Portuguese");
