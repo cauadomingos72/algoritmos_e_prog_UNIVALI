592aba3807cda4b7d5d0f003a86f1998e2b9256e
Paulo Martino Hermans <phermans@edu.univali.br>
2023-03-27 12:44:16 -0300

    Create musicas.h

---
+++ ListaGenerica_C++/Musicas/musicas.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <locale.h>
+#include <windows.h>
+#include <stdlib.h>
+#include "listaDuplamenteEncadeada.h"
+
+using namespace std;
+
+struct musica
+{
+    string nome;
+    string artista;
+    int duracao;
+};
+
+
+void aleatorio(listaDEnc<musica> &playlist)
+{
+    
+}
