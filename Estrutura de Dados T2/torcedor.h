76d21d1870e0dd04035ad84a5d80bbc60ab64867
Cauã Domingos <102312253+cauadomingos72@users.noreply.github.com>
2023-05-08 12:49:34 -0300

    Update torcedor.h

--- Estrutura de Dados T2/torcedor.h
+++ Estrutura de Dados T2/torcedor.h
@@ -1,6 +1,26 @@
 #ifndef TORCEDOR_H_INCLUDED
 #define TORCEDOR_H_INCLUDED
 
+struct Torcedor
+{
+  int nome;
+  int tempo;
+  bool socio;
+};
 
+void addNome(Torcedor a)
+{
+  
+}
+
+int contTempo()
+{
+  return 0;
+}
+
+bool setSocio(Torcedor a)
+{
+  return a.socio = true;
+}
 
 #endif // TORCEDOR_H_INCLUDED
