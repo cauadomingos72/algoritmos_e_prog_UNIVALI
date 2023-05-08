cd3928d2f43820a17ee1d4d1dde8fe44c81424e2
Paulo Martino Hermans <phermans@edu.univali.br>
2023-05-08 19:58:36 -0300

    Update main.cpp

--- Estrutura de Dados T2/main.cpp
+++ Estrutura de Dados T2/main.cpp
@@ -5,38 +5,38 @@
 
 using namespace std;
 
-struct guiches{
-    int num;
-    int total;
-    Fila<Torcedor> gfila;
-    guiches *proximo;
+struct guiches{    ///Criação do tipo Guichê
+    int num;    ///Variável que descreve o numero referente ao guichê
+    int total;    ///Variável que indica o total de pessoas aguardando atendimento
+    Fila<Torcedor> gfila;    ///Englobamento da fila de espera do guichê
+    guiches *proximo;    ///Ponteiro para o guichê ao lado
 };
 
 struct gnormal{
-    guiches *primeiroN;
+    guiches *primeiroN;    ///Criação de uma lista de guichês normais
 };
 
 struct gsocios{
-    guiches *primeiroS;
+    guiches *primeiroS;    ///Criação de uma lista de guichês para sócios
 };
 
-guiches *novoGuiche(int num){
-    guiches *novoG = new guiches;
-    novoG->num = num;
-    novoG->total = 0;
-    iniciaFila(novoG->gfila);
-    novoG->proximo = NULL;
+guiches *novoGuiche(int num){    ///Função para criação de um novo guichê
+    guiches *novoG = new guiches;    ///Declaração de guichê
+    novoG->num = num;    ///Atribuição do numero referente
+    novoG->total = 0;    ///Inicialização do guichê com zero na espera
+    iniciaFila(novoG->gfila);    ///Inicialização da fila do guichê
+    novoG->proximo = NULL;   ///Preparação do ponteiro
 }
 
-bool iniciaGNormal(gnormal &g){
+bool iniciaGNormal(gnormal &g){    ///Inicialização da lista de guichês normais
     g.primeiroN = NULL;
 }
 
-bool iniciaGSocios(gsocios &g){
+bool iniciaGSocios(gsocios &g){    ///Inicialização da lista de guichês para sócios
     g.primeiroS = NULL;
 }
 
-void menu(int &GS, int &GN, int &C, int &P, int &T) ///Função de exibição e seleção de opções.
+void menu(int &GS, int &GN, int &C, int &P, int &T) ///Função de seleção das opções da simulação
 {
     system("cls");
     cout<<"Qtd. de guichês sócio-torcedor: "<<endl;
