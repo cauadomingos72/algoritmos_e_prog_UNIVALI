bd063030faef7beeec45970ca6ceb2f7c8030fd3
Cauã Domingos <102312253+cauadomingos72@users.noreply.github.com>
2023-05-08 07:59:10 -0300

    Update main.asm

--- Arquitetura_e_Organização_de_Processadores/AV3/main.asm
+++ Arquitetura_e_Organização_de_Processadores/AV3/main.asm
@@ -1,48 +1,47 @@
 # Disciplina: Arquitetura e Organização de Processadores
-# Atividade: Avaliação 03 – Programação em Linguagem de Montagem
-# Aluno: Caua Domingos e Taryck Santos
-
+# Atividade: Avaliação 03 – Programação de Procedimentos
+# Alunos: Caua Domingos e Taryck Santos
 .data #Segmento de Dados
-
 	Msg1: .asciz "\n\n Digite um valor para X: "
 	Msg2: .asciz "\n\n Digite um valor para Y: "
 	Msg3: .asciz "\n\n Resultado do MDC: "
-	
+	Msg4: .asciz "\n\n Apenas números maiores ou iguais a1!"
 .text #Segmento de Texto
-
 global:
-
 	j main
 	
 mdc:
-
-	addi sp, sp, -4 #Aloca espaço na pilha
-	sw ra, 4(sp) #Salva o endereço de retorno na pilha
-	sw s1, 0(sp) #Salva o registrador s1 na pilha
+	addi sp, sp, -12 # Aloca espaço na pilha
+	sw ra, 8(sp) # Salva o endereço de retorno na pilha
+	sw a1, 4(sp) # Salva o registrador a1 na pilha
+	sw a2, 0(sp) # Salva o registrador a2 na pilha
 	
 	while:
-	
-		beq s1, s2, end_while
-		blt s1, s2, if_lt
-		
-		sub s1, s1, s2
-		j while
+		beq a1, a2, end_while # Se a1 e a2 forem iguais, pula para o end_while
+		blt a1, a2, if_lt # se a2 for maior que a1, pula para o if_lt
+		sub a1, a1, a2 # Subtrai de a1 o valor de a2
+		j while # Volta para o while
 		
 	if_lt:
-	
-		sub s2, s2, s1
-		j while
+		sub a2, a2, a1 # Subtrai de a2 o valor de a1
+		j while # Volta para o while
 		
 	end_while:
+		mv s7, a1 # Move para s7 o valor de a1
+		lw ra, 8(sp) # Move para ra o valor dessa posição da pilha
+		lw a1, 4(sp) # Move para a1 o valor dessa posição da pilha
+		lw a2, 0(sp) # Move para a2 o valor dessa posição da pilha
+		addi sp, sp, 12 # Desaloca o espaço na pilha
+		jr ra # Salta para o endereço armazenado em ra
+		
+erro:
+#Mensagem de erro
+	li a7, 4 # Chama o serviço 4 (print_string)
+	la a0, Msg4 # Msg4
+	ecall
 	
-		mv s7, s1
-		lw ra, 4(sp)
-		lw s0, 0(sp)      
-        addi sp, sp, 4        
-        jr ra               
-
 main:
-
+	li t0, 1 # Carrega a constante 1 em t0
 	#Armazenando um valor para X
 	li a7, 4 # Chama o serviço 4 (print_string)
 	la a0, Msg1 # Msg1
@@ -51,7 +50,8 @@ main:
 	li a7, 5 # Chama o serviço 5 (read_int)
 	ecall
 	
-	mv s1, a0 # Move o inteiro para o endereço s1
+	mv a1, a0 # Move o inteiro para o endereço a1
+	blt a1, t0, erro # Caso a1 seja menor que 1, pula para a função erro
 	
 	#Armazenando um valor para Y
 	li a7, 4 # Chama o serviço 4 (print_string)
@@ -61,16 +61,15 @@ main:
 	li a7, 5 # Chama o serviço 5 (read_int)
 	ecall
 	
-	mv s2, a0 # Move o inteiro para o endereço s2
-	
-	jal mdc
+	mv a2, a0 # Move o inteiro para o endereço a2
+	blt a2, t0, erro # Caso a1 seja menor que 1, pula para a função erro
+	jal mdc # Salta para a função mdc
 	
 	#Mostra a Msg3
-	li a7, 4 
-	la a0, Msg3 
+	li a7, 4
+	la a0, Msg3
 	ecall
 	
-	# Print na constante s0
+	# Print na constante s7
 	li a7, 1
-	mv a0, s7 
-	ecall
+	mv a0, 
