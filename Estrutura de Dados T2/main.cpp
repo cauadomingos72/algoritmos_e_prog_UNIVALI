508ff68d2bed45671261c12e98b31ecac6b70c7d
Paulo Martino Hermans <phermans@edu.univali.br>
2023-05-09 22:53:29 -0300

    Update main.cpp

--- Estrutura de Dados T2/main.cpp
+++ Estrutura de Dados T2/main.cpp
@@ -30,10 +30,12 @@ guiches *novoGuiche(int num){    ///Função para criação de um novo guichê
 
 bool iniciaGNormal(gnormal &g){    ///Inicialização da lista de guichês normais
     g.primeiroN = NULL;
+    return true;
 }
 
 bool iniciaGSocios(gsocios &g){    ///Inicialização da lista de guichês para sócios
     g.primeiroS = NULL;
+    return true;
 }
 
 void menu(int &GS, int &GN, int &C, int &P, int &T) ///Função de seleção das opções da simulação
@@ -50,6 +52,34 @@ void menu(int &GS, int &GN, int &C, int &P, int &T) ///Função de seleção das
     cout<<"Qtd. de tempo da simulação: "<<endl;
     cin >> T;
     system("cls");
+}
+
+bool addGNormal(gnormal Normal, int nome, int tempo){
+    if(Normal.primeiroN==NULL)
+        return false;
+    guiches *nav = Normal.primeiroN;
+    int aux = nav->total;
+    
+    
+    
+    Torcedor novo;
+    novo.nome = nome;
+    novo.tempo = tempo;
+    novo.socio = false;
+}
+
+bool addGSocios(gsocios Socios ,int nome, int tempo){
+    if(Socios.primeiroS==NULL)
+        return false;
+    guiches *nav = Socios.primeiroS;
+    int aux = nav->total;
+    
+    
+    
+    Torcedor novo;
+    novo.nome = nome;
+    novo.tempo = tempo;
+    novo.socio = true;
 }
 
 int main()
@@ -70,6 +100,24 @@ int main()
         novo->proximo = Socios.primeiroS;
         Socios.primeiroS = novo;
     }
+    for(int i=1; i<=qtdCarga; i++){
+        int j = 0;
+        if(i%20!=0){
+            if((i%20) < 10)
+                addGNormal(Normal, i-j, 3);
+            if((i%20) < 16)
+                addGNormal(Normal, i-j, 2);
+            if((i%20) < 20)
+                addGNormal(Normal, i-j, 1);
+        }else{
+            j++;
+            if((j%20) < 17)
+                addGSocios(Socios, j, 1);
+            if((j%20) < 20)
+                addGSocios(Socios, j, 2);
+        }
+    }
+
     return 0;
 }
 /* balanceamento de procentagem: 1-((1+x)/x%)
