7bf0240375c0865ec08e48ce655589fb3e3d171f
Cauã Domingos <102312253+cauadomingos72@users.noreply.github.com>
2023-05-10 13:11:14 -0300

    Update pessoas.h

--- POO2/Header/pessoas.h
+++ POO2/Header/pessoas.h
@@ -1,6 +1,20 @@
 #ifndef PESSOAS_H_INCLUDED
 #define PESSOAS_H_INCLUDED
+#include <iostream>
 
+class Pessoas
+{
+  std::string nome;
+  int cpf;
 
+public:
+
+  
+};
+
+class Funcionario::Pessoas
+{
+  
+};
 
 #endif
