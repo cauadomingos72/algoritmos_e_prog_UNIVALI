274264d95c3aea84e4bf3932636d91c1ee84fe1c
Cauã Domingos <102312253+cauadomingos72@users.noreply.github.com>
2023-05-12 12:55:20 -0300

    Update produtos.h

--- POO2/Header/produtos.h
+++ POO2/Header/produtos.h
@@ -1,6 +1,25 @@
 #ifndef PRODUTO_H_INCLUDED
 #define PRODUTO_H_INCLUDED
 
+class Produto
+{
+  bool tipo; //false = musica, true = instrumento
+  int preco;
+  int qtd;
+  int codBarras;
 
+public:
+    
+};
+
+class Musica : public Produto
+{
+  
+};
+
+class Instrumento : public Produto
+{
+
+};
 
 #endif
