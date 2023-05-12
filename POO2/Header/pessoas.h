fc29c980cb388df63cfacc8c671b40a53b0dba52
Cauã Domingos <102312253+cauadomingos72@users.noreply.github.com>
2023-05-12 12:49:00 -0300

    Update pessoas.h

--- POO2/Header/pessoas.h
+++ POO2/Header/pessoas.h
@@ -9,12 +9,41 @@ class Pessoas
 
 public:
 
+  std::string addNome(std::string nome);
+  void printNome(std::string nome);
+  int addCPF(int cpf);
+  void printCPF(int cpf);
+
+  Pessoas (std::string nome, int cpf);
+
   
 };
 
-class Funcionario::Pessoas
+class Funcionarios : public Pessoas
 {
-  
+  std::string funcao;
+  int salario;
+
+public:
+
+  std::string addFuncao(std::string);
+  void printFuncao(std::string);
+  int addSalario(int salario);
+  void printSalario(int salario);
+
+  Funcionarios(std::string funcao, int salario);
+};
+
+class Cliente : public Pessoas
+{
+  int numCadastro;
+
+public:
+
+  int addNumCad(int numCadastro);
+  void printNumCad(int numCadastro);
+
+  Cliente(int numCadastro);
 };
 
 #endif
