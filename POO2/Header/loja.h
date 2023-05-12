1907626bac69ff71cd02d7932323583852e7fd0e
Cauã Domingos <102312253+cauadomingos72@users.noreply.github.com>
2023-05-12 12:49:32 -0300

    Update loja.h

--- POO2/Header/loja.h
+++ POO2/Header/loja.h
@@ -1,6 +1,7 @@
 #ifndef LOJA_H_INCLUDED
 #define LOJA_H_INCLUDED
 #include "pessoas.h"
+#include "produtos.h"
 
 class Loja
 {
@@ -10,6 +11,12 @@ class Loja
 
 public:
 
+  Pessoas addClient(Pessoas client);
+  void printClient(Pessoas client);
+  Pessoas addFuncio(Pessoas funcio);
+  Pessoas printFuncio(Pessoas funcio);
+  Produto addProd(Produto item);
+  Produto printProd(Produto item);
   
 };
 
