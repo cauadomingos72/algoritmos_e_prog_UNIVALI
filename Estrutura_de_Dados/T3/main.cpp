26339299cc20032ca4da4cafb6e5e0c940f452d6
Cauã Domingos <102312253+cauadomingos72@users.noreply.github.com>
2023-06-09 10:18:01 -0300

    change main.cpp t3

--- Estrutura_de_Dados/T3/main.cpp
+++ Estrutura_de_Dados/T3/main.cpp
@@ -1,9 +1,31 @@
+// Alunos:
+
 #include <iostream>
 #include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "BinaryTree.h"
 
+using namespace std;
+
+void menu(int &op)
+{
+  cout<<"-----------------------------------\t\t";
+  cout<<"\n\nÁrvore Binária de Expressão\t\t";
+  cout<<"-----------------------------------\t";
+  cout<<"1 - Inserir Expressão"<<endl;
+  cout<<"2 - Imprimir Árvore"<<endl;
+  cout<<"3 - Resultado"<<endl;
+  cout<<"4 - Encerrar"<<endl;
+  cin>>op;
+}
+
 int main() {
   setlocale(LC_ALL, "Portuguese");
+  int op, run = 1;
+
+  do
+  {
+    menu(op);
+  } while (run == 1);
 }
